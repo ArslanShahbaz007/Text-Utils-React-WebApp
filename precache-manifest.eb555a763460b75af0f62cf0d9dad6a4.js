f4e4946284144e7a9c9a45909d6610d730e0a014
ArslanShahbaz007 <ranaarslanshahbaz@gmail.com>
2023-12-11 17:36:32 +0500

    Updates

---
+++ precache-manifest.eb555a763460b75af0f62cf0d9dad6a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f07ff7cf352c78ab7e213a3b15abcc4a",
+    "url": "/Text-Utils-React-WebApp/index.html"
+  },
+  {
+    "revision": "9a031e03dabc0482857a",
+    "url": "/Text-Utils-React-WebApp/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "8bca827d70f3d8b1f1bd",
+    "url": "/Text-Utils-React-WebApp/static/js/2.2edda431.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Text-Utils-React-WebApp/static/js/2.2edda431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82594277ffdbbbbe6c96",
+    "url": "/Text-Utils-React-WebApp/static/js/3.f00e9902.chunk.js"
+  },
+  {
+    "revision": "9a031e03dabc0482857a",
+    "url": "/Text-Utils-React-WebApp/static/js/main.e9efb487.chunk.js"
+  },
+  {
+    "revision": "a3e0d08d2f12b2faa3a2",
+    "url": "/Text-Utils-React-WebApp/static/js/runtime-main.3ebd9a15.js"
+  }
+]);
